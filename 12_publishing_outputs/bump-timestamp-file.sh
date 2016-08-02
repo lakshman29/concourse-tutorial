dadd71dd5c9547ab259f5aa81168575697f191c7
lakshman29 <lakshman29@gmail.com>
2016-08-02 14:59:51 +0530

    commented the git push commands

--- 12_publishing_outputs/bump-timestamp-file.sh
+++ 12_publishing_outputs/bump-timestamp-file.sh
@@ -8,11 +8,11 @@ git clone resource-tutorial updated-gist
 #cd updated-gist
 #echo $(date) > bumpme
 
-git config --global user.email "lakshman29@gmail.com"
-git config --global user.name "lakshman29"
+#git config --global user.email "lakshman29@gmail.com"
+#git config --global user.name "lakshman29"
 
-git add .
-git commit -m "Bumped date"
-git remote add origin https://github.com/lakshman29/practice-web.git
-git remote -v 
-git push -u origin master
+#git add .
+#git commit -m "Bumped date"
+#git remote add origin https://github.com/lakshman29/practice-web.git
+#git remote -v 
+#git push -u origin master
