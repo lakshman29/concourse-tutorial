6273d287d377092adc5241a33562f31a2fbcfb37
lakshman29 <lakshman29@gmail.com>
2016-08-02 14:51:05 +0530

    added the -v

--- 12_publishing_outputs/bump-timestamp-file.sh
+++ 12_publishing_outputs/bump-timestamp-file.sh
@@ -5,7 +5,7 @@ set -x # print commands
 
 git clone resource-tutorial updated-gist
 
-#cd updated-gist
+cd updated-gist
 #echo $(date) > bumpme
 
 git config --global user.email "lakshman29@gmail.com"
@@ -14,4 +14,5 @@ git config --global user.name "lakshman29"
 git add .
 git commit -m "Bumped date"
 git remote add origin https://github.com/lakshman29/practice-web.git
+git remote -v 
 git push -u origin master
