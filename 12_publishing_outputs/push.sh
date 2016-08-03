b9e29a162c243e2eb05969c1ae2a283fcce29550
lakshman29 <lakshman29@gmail.com>
2016-08-03 17:59:57 +0530

    mvn quickstart

--- 12_publishing_outputs/push.sh
+++ 12_publishing_outputs/push.sh
@@ -2,11 +2,4 @@
 
 set -e # fail fast
 set -x # print commands
-
-
-
-git config --global user.email "nobody@concourse.ci"
-git config --global user.name "Concourse"
-
-git add .
-git commit -m "Bumped date"
+mvn archetype:generate -Dfilter=org.apache.maven.archetypes:maven-archetype-quickstart
