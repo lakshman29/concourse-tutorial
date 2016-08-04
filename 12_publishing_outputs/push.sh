249c22ad79b709c375957b252f5598a22bd7feca
lakshman29 <lakshman29@gmail.com>
2016-08-04 19:37:57 +0530

    Update push.sh

--- 12_publishing_outputs/push.sh
+++ 12_publishing_outputs/push.sh
@@ -3,3 +3,4 @@
 set -e # fail fast
 set -x # print commands
 mvn archetype:generate -Dfilter=org.apache.maven.archetypes:maven-archetype-quickstart
+mvn archetype:create -DgroupId=com.test -DartifactId=DEF
