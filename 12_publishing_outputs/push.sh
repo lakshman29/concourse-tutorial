be86c16ac0d8ee66ac54a8b08270ee3982e8982e
lakshman29 <lakshman29@gmail.com>
2016-08-05 15:03:42 +0530

    added the push

--- 12_publishing_outputs/push.sh
+++ 12_publishing_outputs/push.sh
@@ -4,3 +4,12 @@ set -e # fail fast
 set -x # print commands
 #mvn archetype:generate -Dfilter=org.apache.maven.archetypes:maven-archetype-quickstart
 mvn archetype:generate -DgroupId=com.test -DartifactId=DEF -Dpackaging=jar -Dversion=1.0 -DinteractiveMode=false
+
+git config --global user.email "lakshman29@gmail.com"
+git config --global user.name "lakshman29"
+
+git add .
+git commit -m "Bumped date"
+git remote add origin https://github.com/lakshman29/practice-web.git
+git remote -v 
+git push -u origin master
